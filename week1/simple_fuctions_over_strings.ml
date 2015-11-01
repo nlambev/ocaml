89c64a3cbcf3fbaacca32013192e840c413785ad
Nikola Lambev <nlambev@users.noreply.github.com>
2015-11-01 18:23:22 +0000

    Fixed formatting.

--- week1/simple_fuctions_over_strings.ml
+++ week1/simple_fuctions_over_strings.ml
@@ -1,7 +1,11 @@
 
 (*
-  1) multiple_of that takes two integer parameters, n and d, and determines whether n is a multiple of d. The function must return a boolean value. This function can be written without recursion. Look at the operators defined on integers in sequence 1.
-  2) integer_square_root that calculates the integer square root of a positive integer n, that is the largest integer r such that r * r <= n. Hint: you may use floating point arithmetic, but don't forget that you have to convert explicitely between float and int.
+  1) multiple_of that takes two integer parameters, n and d, and determines whether n is a multiple of d. 
+  The function must return a boolean value. This function can be written without recursion. Look at the 
+  operators defined on integers in sequence 1.
+  2) integer_square_root that calculates the integer square root of a positive integer n, that is the 
+  largest integer r such that r * r <= n. Hint: you may use floating point arithmetic, but don't forget 
+  that you have to convert explicitely between float and int.
   3) last_character that returns the last character of a string, assuming that the string argument is not empty;
   4) string_of_bool that converts a boolean value to its string representation.
 *)
